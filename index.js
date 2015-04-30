e861f8810455cb66552e8561cdfe1934f30aa504
anvaka <anvaka@gmail.com>
2015-04-30 01:37:57 -0700

    Generalized to n-dimensional layout

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ createLayout.get2dLayout = require('ngraph.forcelayout');
 function createLayout(graph, physicsSettings) {
   var merge = require('ngraph.merge');
   physicsSettings = merge(physicsSettings, {
-        createQuadTree: require('./lib/orthantTree/index.js'),
-        createBounds: require('./lib/bounds'),
-        createDragForce: require('./lib/dragForce'),
-        createSpringForce: require('./lib/springForce'),
-        integrator: require('./lib/eulerIntegrator'),
-        createBody: require('./lib/createBody')
-      });
+    dimension: 2,
+    createQuadTree: require('./lib/orthantTree/index.js'),
+    createBounds: require('./lib/bounds'),
+    createDragForce: require('./lib/dragForce'),
+    createSpringForce: require('./lib/springForce'),
+    integrator: require('./lib/eulerIntegrator'),
+    createBody: require('./lib/createBody')
+  });
 
   return createLayout.get2dLayout(graph, physicsSettings);
 }
