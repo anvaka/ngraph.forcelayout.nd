991df04d9bc83adef8a8caac32e6d388b90b6e64
Andriy Kashcha <anvaka@gmail.com>
2020-08-30 14:36:03 -0700

    updated perf with true 2d layout

--- perf/index.js
+++ perf/index.js
@@ -1,11 +1,14 @@
 var graph = require('ngraph.generators').grid(20, 20);
+var trueForceLayout = require('ngraph.forcelayout');
 var iterationsPerRun = 20;
+var createNDLayout = require('../');
 
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
 // add tests
-suite.add('Run 2d layout', createLayoutPerfTest(2))
+suite.add('Real 2d layout', real2DLayoutPerfTest)
+.add('Run 2d layout', createLayoutPerfTest(2))
 .add('Run 3d layout', createLayoutPerfTest(3))
 .add('Run 4d layout', createLayoutPerfTest(4))
 .on('cycle', function(event) {
@@ -19,11 +22,18 @@ suite.add('Run 2d layout', createLayoutPerfTest(2))
 
 function createLayoutPerfTest(dimension) {
   return function () {
-    var layout = require('../')(graph, {
+    var layout = createNDLayout(graph, {
       dimension: dimension
     });
     for (var i = 0; i < iterationsPerRun; ++i) {
       layout.step();
     }
   };
+}
+
+function real2DLayoutPerfTest() {
+    var layout = trueForceLayout(graph);
+    for (var i = 0; i < iterationsPerRun; ++i) {
+      layout.step();
+    }
 }
