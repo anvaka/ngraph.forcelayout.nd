9f9802ff42ca11fe47176c406245914e6ca9ac77
Andriy Kashcha <anvaka@gmail.com>
2020-09-06 00:15:12 -0700

    added perf test for new forcelayout

--- perf/index.js
+++ perf/index.js
@@ -7,7 +7,10 @@ var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
 // add tests
-suite.add('Real 2d layout', real2DLayoutPerfTest)
+suite
+.add('Real 2d layout', createNewForceLayoutPerfTest(2))
+.add('Real 3d layout', createNewForceLayoutPerfTest(3))
+.add('Real 4d layout', createNewForceLayoutPerfTest(4))
 .add('Run 2d layout', createLayoutPerfTest(2))
 .add('Run 3d layout', createLayoutPerfTest(3))
 .add('Run 4d layout', createLayoutPerfTest(4))
@@ -31,9 +34,11 @@ function createLayoutPerfTest(dimension) {
   };
 }
 
-function real2DLayoutPerfTest() {
-    var layout = trueForceLayout(graph);
+function createNewForceLayoutPerfTest(dimensions) {
+  return function() {
+    var layout = trueForceLayout(graph, {dimensions: dimensions});
     for (var i = 0; i < iterationsPerRun; ++i) {
       layout.step();
     }
+  }
 }
